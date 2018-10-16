84a305dcc5ef0d4bfc0ff890e9fab2a5a7da4e8b
david <davidvc@gmail.com>
2018-10-16 13:21:30 +0200

    einde les

--- dobbelstenen/src/sample/Controller.java
+++ dobbelstenen/src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import model.Game;
+import view.PlayerView;
+import view.ScoreView;
+
 public class Controller {
+    public Controller() {
+        PlayerView pv3 = new PlayerView(3);
+        PlayerView pv2 = new PlayerView(2);
+        PlayerView pv1 = new PlayerView(1);
+        ScoreView scoreview = new ScoreView();
+        Game game = new Game();
+
+        game.registerObserver(pv3);
+        game.registerObserver(pv2);
+        game.registerObserver(pv1);
+
+        game.registerObserver(scoreview);
+    }
 }
